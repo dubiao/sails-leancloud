86dd04e2c66381bcc4f9ea6fc16efae98b93af4e
Jimmy.Liu <hero63418@gmail.com>
2017-02-03 11:21:54 +0800

    Bumped version number

--- tools/tasks/seed/release/npm.publish.public.ts
+++ tools/tasks/seed/release/npm.publish.public.ts
@@ -2,7 +2,7 @@ import { exec } from 'child_process';
 
 export = (done: any) => {
 
-  let command = 'npm publish --access=public';
+  let command = 'npm publish --access=public --registry=https://registry.npmjs.org';
 
   // Only deletes tags locally, not remote
   exec(command, done);
